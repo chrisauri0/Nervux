e5e04cf62a87533c989006f787f0478c3ef89bbe
chrisauri0 <chaparitoveb20@gmail.com>
2025-03-03 17:26:00 -0600

    possible final commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#010119",
+        secondary: "#1b56ac",
+        accent: "#0de7fc",
+        neutral: "#a6a6a6",
+        light: "#e7f1f8",
+        white: "#ffffff",
+        black: "#000000",
+      },
+      fontFamily: {
+        sans: [
+          "Glacial Indifference",
+          "Roboto",
+          "League Spartan",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+};
