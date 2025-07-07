7f2c24f5ecd120a32f04e45151fcd71329a54441
chrisauri0 <chaparitoveb20@gmail.com>
2025-07-06 18:23:25 -0600

    try to update a seo with < meta />

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "NERVUX Neurotechnology";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Clínica especializada en neuroestimulación y bienestar mental en Querétaro. Descubre tratamientos innovadores para tu salud cerebral!";
